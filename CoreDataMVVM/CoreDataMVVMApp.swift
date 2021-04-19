1908ea728cad634d8d30ad1667f3373375d55493
Mohammad Azam <azamsharp@gmail.com>
2021-04-19 12:54:08 -0500

    Initial Commit

---
+++ CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by Mohammad Azam on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
